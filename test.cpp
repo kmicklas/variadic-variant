35b7ffab668cafbea7c0e6d72d26ecf6baa4b710
Kenneth Micklas <kmicklas@gmail.com>
2013-03-01 00:08:02 -0500

    Test destruction

--- test.cpp
+++ test.cpp
@@ -7,12 +7,26 @@
 using namespace std;
 using namespace variant;
 
+struct has_destructor {
+	bool& flag;
+	
+	has_destructor(bool& flag) : flag(flag) {}
+	
+	~has_destructor() {
+		flag = true;
+	}
+};
+
 int main(int argc, char *argv[]) {
-	Variant<int, bool, double, string> test(4);
+	bool destructed = false; // has our object been destructed?
+	
+	Variant<int, bool, double, string, has_destructor> test(4);
 	assert(test.tag() == 0);
 	assert(test.get<int>() == 4);
 	
+	test = has_destructor(destructed);
 	test = string("a");
+	assert(destructed); // assignment should call destructor of previous object
 	assert(test.tag() == 3);
 	assert(test.get<string>() == "a");
 	
